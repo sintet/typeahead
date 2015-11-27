b16653a11b52990527117342eee74ab7ac9031a4
Dzmitry Bahachuk <mayrog87@gmail.com>
2015-11-27 17:58:58 +0300

    changes

--- js/app.js
+++ js/app.js
@@ -1,4 +1,38 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  'use strict';
+  var minCharLength = 3;
+  var mock = [{
+    name: "asd",
+    tags: "asd aaa ddd",
+    desc: "loremasdasdasdasdasdasd"
+  }, {
+    name: "qwe",
+    tags: "qwe www eee",
+    desc: "assss"
+  }, {
+    name: "zxc",
+    tags: "zxc zzz ccc",
+    desc: "zxczxcacsasc"
+  }];
+
+  $("#typeaheadInput").on('keyup',function () {
+
+    if($(this).val().length >= minCharLength ){
+      console.log($(this).val());
+      $(".dropdown").removeClass('hidden');
+    }
+
+  });
+
+
+  $("#typeaheadInput").on('blur', function () {
+    $(".dropdown").addClass('hidden');
+  });
+
+  $(".dropdown").find(".itm").on('click', function () {
+    var val = $(this).html();
+    $("#typeaheadInput").val(val)
+  });
 
 
 });
